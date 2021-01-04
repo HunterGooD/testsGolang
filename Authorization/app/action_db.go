69990cc0a3723f9f7a083db846a1be55d84ef5e0
Gudov Vladislav <vlad.gudov@inbox.ru>
2021-01-04 08:24:19 +0400

    db

--- Authorization/app/action_db.go
+++ Authorization/app/action_db.go
@@ -1,6 +1,7 @@
 package app
 
 // CreateUser создает пользователя в базе данных
-func (a *App) createUser(login, passwordHash string) {
-	a.db.Exec("INSERT INTO `user` (`login`, `password`) VALUES ($1, $2)", login, passwordHash)
+func (a *App) createUser(login, passwordHash string) error {
+	_, err := a.db.Exec("INSERT INTO `user` (`login`, `password`) VALUES ($1, $2)", login, passwordHash)
+	return err
 }
